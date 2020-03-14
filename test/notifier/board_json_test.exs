766fb5287d4125f55270ffba42a89820e3b856b1
Cassio Godinho <cassiopgodinho@gmail.com>
2020-03-14 10:14:55 -0300

    fix some tests

--- test/notifier/board_json_test.exs
+++ test/notifier/board_json_test.exs
@@ -6,6 +6,7 @@ defmodule Notifier.BoardJsonTest do
   describe "cards" do
     alias Notifier.Board.Card
 
+    @valid_column_attrs %{name: "TODO", id: 123}
     @valid_attrs %{body: "some body", column_id: 123}
     @update_attrs %{body: "some updated body"}
     @invalid_attrs %{body: nil}
@@ -19,18 +20,29 @@ defmodule Notifier.BoardJsonTest do
       card
     end
 
+    def column_fixture(attrs \\ %{}) do
+      {:ok, column} =
+        attrs
+        |> Enum.into(@valid_column_attrs)
+        |> Board.create_column()
+      column
+    end
+
     test "list_cards/0 returns all cards" do
-      card = card_fixture()
+      column = column_fixture()
+      card = card_fixture(%{column_id: column.id})
       assert Board.list_cards() == [card]
     end
 
     test "get_card!/1 returns the card with given id" do
-      card = card_fixture()
+      column = column_fixture()
+      card = card_fixture(%{column_id: column.id})
       assert Board.get_card!(card.id) == card
     end
 
     test "create_card/1 with valid data creates a card" do
-      assert {:ok, %Card{} = card} = Board.create_card(@valid_attrs)
+      column = column_fixture()
+      assert {:ok, %Card{} = card} = Board.create_card(%{body: "some body", column_id: column.id})
       assert card.body == "some body"
     end
 
@@ -39,25 +51,29 @@ defmodule Notifier.BoardJsonTest do
     end
 
     test "update_card/2 with valid data updates the card" do
-      card = card_fixture()
+      column = column_fixture()
+      card = card_fixture(%{column_id: column.id})
       assert {:ok, %Card{} = card} = Board.update_card(card, @update_attrs)
       assert card.body == "some updated body"
     end
 
     test "update_card/2 with invalid data returns error changeset" do
-      card = card_fixture()
+      column = column_fixture()
+      card = card_fixture(%{column_id: column.id})
       assert {:error, %Ecto.Changeset{}} = Board.update_card(card, @invalid_attrs)
       assert card == Board.get_card!(card.id)
     end
 
     test "delete_card/1 deletes the card" do
-      card = card_fixture()
+      column = column_fixture()
+      card = card_fixture(%{column_id: column.id})
       assert {:ok, %Card{}} = Board.delete_card(card)
       assert_raise Ecto.NoResultsError, fn -> Board.get_card!(card.id) end
     end
 
     test "change_card/1 returns a card changeset" do
-      card = card_fixture()
+      column = column_fixture()
+      card = card_fixture(%{column_id: column.id})
       assert %Ecto.Changeset{} = Board.change_card(card)
     end
   end
